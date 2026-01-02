ba11734b8d719c818ef36c9d3ab2bf53fae5bb70
theverysameliquidsnake <liquidsnake171@tutamail.com>
2026-01-02 03:08:36 +0200

    test ui

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,17 @@ import { ConfigModule } from '@nestjs/config';
 import { PostgresModule } from './postgres/postgres.module';
 import { TagModule } from './tag/tag.module';
 import { SteamModule } from './steam/steam.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-    imports: [ConfigModule.forRoot({ isGlobal: true }), PostgresModule, FileModule, TagModule, SteamModule],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'webui') }),
+        PostgresModule,
+        FileModule,
+        TagModule,
+        SteamModule,
+    ],
 })
 export class AppModule {}
